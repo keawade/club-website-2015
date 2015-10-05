9f9dfc7ff6fbd33d1891ef2501d7a39bb60d837a
Keith Wade <keawade@gmail.com>
2015-10-05 03:31:39 +0000

    Implemented nodemailer

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 
+var gmailauth = require('./auth.json');
 var data = require('./data');
 
 // Initialize our express application
@@ -23,6 +25,33 @@ app.get('/', function(req,res,next){
         officers: data.officers,
         games: data.games
     });
+});
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: gmailauth
+});
+
+app.post('/email', function(req,res,next){
+  console.log(req.body);
+  var mailOptions = {
+      from: 'UnionTTC <uctechtabletop@gmail.com>',
+      to: 'uctechtabletop@gmail.com',
+      subject: 'Feedback from website',
+      html: '<ul>'+
+              '<li>From: '+req.body.email+'</li>'+
+              '<li>Message: '+req.body.message+'</li>'+
+            '</ul>'
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+    //If you felt like it, you could do fancy renders here, or a thank
+    //you page or something.
+    res.redirect('/');
+  });
 });
 
 var server = app.listen(app.get('port'), app.get('ip'), function() {
